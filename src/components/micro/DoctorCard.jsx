28af766d7cbf0099190cb0faca023cf04eef22eb
codegrann <renderer721@gmail.com>
2024-12-14 16:24:46 +0300

    .

--- src/components/micro/DoctorCard.jsx
+++ src/components/micro/DoctorCard.jsx
@@ -2,10 +2,10 @@ const DoctorCard = ({ name, specialty, rating, hospitalName, department }) => {
     return (
       <div className="p-4 border rounded shadow-md md:w-[700px]">
         <h3 className="text-lg font-bold">{name}</h3>
-        <p className="text-sm text-gray-600">Hospital: {hospitalName}</p>
-        <p className="text-sm text-gray-600">Department: {department}</p>
-        <p className="text-sm text-gray-600">Specialty: {specialty}</p>
-        <p className="text-sm text-gray-600">Rating: {rating} ⭐</p>
+        <p className="text-sm text-gray-600">병원: {hospitalName}</p> {/*Hospital*/}
+        <p className="text-sm text-gray-600">진료과: {department}</p> {/*Department*/}
+        <p className="text-sm text-gray-600">전문분야: {specialty}</p> {/*Specialty*/}
+        <p className="text-sm text-gray-600">평점: {rating} ⭐</p> {/*Rating*/}
       </div>
     );
   };
--- src/components/micro/HospitalCard.jsx
+++ src/components/micro/HospitalCard.jsx
@@ -2,7 +2,7 @@ const HospitalCard = ({ name, rating }) => {
     return (
       <div className="p-4 border rounded shadow-md md:w-[700px]">
         <h3 className="text-lg font-bold">{name}</h3>
-        <p className="text-sm text-gray-600">Rating: {rating} ⭐</p>
+        {/* <p className="text-sm text-gray-600">Rating: {rating} ⭐</p> */}
       </div>
     );
   };
