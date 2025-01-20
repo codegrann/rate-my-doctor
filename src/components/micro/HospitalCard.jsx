1fcf380ca752b155a5956957796ad10a67abd2b1
codegrann <renderer721@gmail.com>
2025-01-21 01:22:07 +0300

    .

--- src/components/micro/HospitalCard.jsx
+++ src/components/micro/HospitalCard.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const HospitalCard = ({ name, rating }) => {
+  const [ratings, setRatings] = useState([]);
+  const [averageRating, setAverageRating] = useState(0);
   const navigate = useNavigate();
+
+  // Fetch ratings when component loads
+  useEffect(() => {
+    fetch(`${BASE_URL}/ratings/${name}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setRatings(data);
+        calculateAverageRating(data); // Calculate average once data is fetched
+      })
+      .catch((error) => console.error('Error fetching ratings:', error));
+  }, [name]);
+
+
+  // Calculate the average rating
+  const calculateAverageRating = (ratingsData) => {
+    if (ratingsData.length === 0) {
+      setAverageRating(0);
+      return;
+    }
+    const total = ratingsData.reduce((sum, rating) => sum + rating.overallRating, 0);
+    const average = total / ratingsData.length;
+    setAverageRating(parseFloat(average.toFixed(1))); // Rounded to one decimal
+  };
 
   const handleClick = () => {
     navigate(`/hospital/${name}`);
@@ -9,6 +35,7 @@ const HospitalCard = ({ name, rating }) => {
     return (
       <div className="p-4 border rounded shadow-md md:w-[700px] cursor-pointer" onClick={handleClick}>
         <h3 className="text-lg font-bold">{name}</h3>
+        <h3 className="text-lg font-bold">No. of ratings: {ratings.length} | Overal Quality: {averageRating} ⭐</h3>
         {/* <p className="text-sm text-gray-600">Rating: {rating} ⭐</p> */}
       </div>
     );
