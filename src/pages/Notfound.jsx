baf7a3371874f2908cbfbc3fda8af60800e120dd
codegrann <renderer721@gmail.com>
2025-01-28 10:41:35 +0300

    .

--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 function NotFound() {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
-      <Link to="/">Go back to Home</Link>
+    <div className='h-screen flex flex-col gap-4 items-center justify-center px-8 text-center'>
+      <h1> <span className='text-red-500 font-bold text-2xl'>404 - Page Not Found</span> </h1>
+      <p className='text-gray-500'>The page you are looking for seems not to work or does not exist.</p>
+      <Link to="/" className='text-blue-600 text-lg'>Go back to Home</Link>
     </div>
   );
 }
