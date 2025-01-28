1622830423bc6cf8782a791fe09c219860d09159
codegrann <renderer721@gmail.com>
2025-01-28 12:07:55 +0300

    .

--- src/components/micro/HospitalCard.jsx
+++ src/components/micro/HospitalCard.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const HospitalCard = ({ name, rating, BASE_URL  }) => {
+const HospitalCard = ({ name, BASE_URL  }) => {
   const [ratings, setRatings] = useState([]);
   const [averageRating, setAverageRating] = useState(0);
   const navigate = useNavigate();
@@ -29,13 +29,25 @@ const HospitalCard = ({ name, rating, BASE_URL  }) => {
     setAverageRating(parseFloat(average.toFixed(1))); // Rounded to one decimal
   };
 
+   // Determine the background color based on the rating range
+   const getBgColor = (rating) => {
+    if (rating >= 0 && rating <= 1) return "bg-red-500";
+    if (rating > 1 && rating <= 2) return "bg-orange-400";
+    if (rating > 2 && rating <= 3) return "bg-yellow-300"; //yellow3
+    if (rating > 3 && rating <= 4) return "bg-green-300";
+    if (rating > 4 && rating <= 5) return "bg-green-500";
+    return "bg-gray-200"; // Fallback color
+  };
+
+  const bgColor = getBgColor(averageRating);
+
   const handleClick = () => {
     navigate(`/hospital/${name}`);
   };
     return (
-      <div className="p-4 border rounded shadow-md md:w-[700px] cursor-pointer" onClick={handleClick}>
-        <h3 className="text-lg font-bold">{name}</h3>
-        <h3 className="text-lg font-bold">No. of ratings: {ratings.length} | Overal Quality: {ratings.length==0? 'Not rated': averageRating} ⭐</h3>
+      <div className="flex items-center gap-8 p-4 border rounded shadow-md md:w-[700px] cursor-pointer" onClick={handleClick}>
+        <div className="text-[9pt] border border-blue-400 flex flex-col items-center w-[10%]">  QUALITY <span className={`${ratings.length > 0 ? `${bgColor} font-bold text-lg border border-green-400 w-full h-14 flex justify-center items-center` : 'font-normal text-[8pt] text-gray-400 my-2'}`}>{ratings.length==0? 'Not rated': averageRating}</span> <span>{ratings.length} ratings <span className='text-yellow-600'>⭐</span> </span> </div>
+        <h3 className="text-lg font-bold border border-red-400">{name}</h3>
         {/* <p className="text-sm text-gray-600">Rating: {rating} ⭐</p> */}
       </div>
     );
